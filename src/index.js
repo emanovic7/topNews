9b2f0de825d50466923db060df1227808372b8ea
github username <github email address>
2019-02-22 16:24:55 -0500

    import compose from redux, use to log actions.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import * as serviceWorker from './serviceWorker';
 import RootReducer from './reducers/RootReducer';
 
-const store = createStore(RootReducer)
+
+
+const store = createStore(
+  RootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
